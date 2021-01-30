6de0c2a9b1426d46c726e583697e681ea9ae8780
alfin2312 <alfin.mulya1999@gmail.com>
2021-01-30 20:44:23 +0700

    backend done

---
+++ app/src/main/java/com/d3/aplikasilogbook/MainActivity.java
@@ -0,0 +1,161 @@
+package com.d3.aplikasilogbook;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView listView;
+    DBHelper helper;
+    LayoutInflater inflater;
+    View dialogView;
+    TextView Tv_tanggal, Tv_kegiatan, Tv_keterangan, Tv_lokasi, Tv_gambar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
+        helper = new DBHelper(this);
+        listView = (ListView)findViewById(R.id.list_data);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setListView(){
+        Cursor cursor = helper.allData();
+        CustomCursorAdapter customCursorAdapter = new CustomCursorAdapter(this, cursor, 1);
+        listView.setAdapter(customCursorAdapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long x) {
+        TextView getId = (TextView)view.findViewById(R.id.list_id);
+        final long id = Long.parseLong(getId.getText().toString());
+        final Cursor cur = helper.oneData(id);
+        cur.moveToFirst();
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Pilih Opsi");
+
+        String[] options = {"Lihat Data","Edit Data", "Hapus Data"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        final AlertDialog.Builder viewData = new AlertDialog.Builder(MainActivity.this);
+                        inflater = getLayoutInflater();
+                        dialogView = inflater.inflate(R.layout.view_data, null);
+                        viewData.setView(dialogView);
+                        viewData.setTitle("Lihat Data");
+
+                        Tv_tanggal = (TextView)dialogView.findViewById(R.id.tv_Tanggal);
+                        Tv_kegiatan = (TextView)dialogView.findViewById(R.id.tv_Kegiatan);
+                        Tv_keterangan = (TextView)dialogView.findViewById(R.id.tv_Keterangan);
+                        Tv_lokasi = (TextView)dialogView.findViewById(R.id.tv_Lokasi);
+                        Tv_gambar = (TextView)dialogView.findViewById(R.id.tv_Gambar);
+
+                        Tv_tanggal.setText("Tanggal: " + cur.getString(cur.getColumnIndex(DBHelper.row_tanggal)));
+                        Tv_kegiatan.setText("Kegiatan: " + cur.getString(cur.getColumnIndex(DBHelper.row_kegiatan)));
+                        Tv_keterangan.setText("Keterangan: " + cur.getString(cur.getColumnIndex(DBHelper.row_keterangan)));
+                        Tv_lokasi.setText("Lokasi: " + cur.getString(cur.getColumnIndex(DBHelper.row_lokasi)));
+                        Tv_lokasi.setText("Bukti Foto: " + cur.getString(cur.getColumnIndex(DBHelper.row_gambar)));
+
+                        viewData.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        viewData.show();
+                }
+                switch (which){
+                    case 1:
+                        Intent iddata = new Intent(MainActivity.this, EditActivity.class);
+                        iddata.putExtra(DBHelper.row_id, id);
+                        startActivity(iddata);
+                }
+                switch (which){
+                    case 2:
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                        builder1.setMessage("Data ini akan dihapus");
+                        builder1.setCancelable(true);
+                        builder1.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                helper.deleteData(id);
+                                Toast.makeText(MainActivity.this, "Data terhapus", Toast.LENGTH_SHORT).show();
+                                setListView();
+                            }
+                        });
+                        builder1.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alertDialog = builder1.create();
+                        alertDialog.show();
+                }
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setListView();
+    }
+}
